95541272ba1d7c2b77624ade553bf0bfecf22d04
dayane112 <71454308+dayane112@users.noreply.github.com>
2024-11-20 10:57:44 -0500

    Created recipeSchema.mjs

---
+++ db/model/userSchema.mjs
@@ -0,0 +1,47 @@
+// import
+import mongoose from 'mongoose';
+
+const recipeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    category: {
+        type: String,
+        enum: ["breakfast", "lunch", "dinner" , "dessert", "baking", "drink"],
+        required: true,
+    },
+    level: {
+        type: String,
+        required: true
+    },
+    serving: {
+        type: Number,
+        required: true,
+    },
+    total: {
+        type: String,
+        required: true
+    },
+    ingredient: [
+        {
+            type: String,
+            required: true
+        },
+    ],
+    direction: [
+        {
+            type: String,
+            required: true,
+        },
+    ],
+    date: { 
+        type: Date, 
+        default: Date.now 
+    },
+});
+
+recipeSchema.index({ name: 1 })
+
+export default mongoose.model('Recipe', recipeSchema);
