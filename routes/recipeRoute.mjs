825685ee2f1cfea2fa01fa214e21a885eecdd4f3
dayane112 <71454308+dayane112@users.noreply.github.com>
2024-11-20 11:05:51 -0500

    Created post route

--- routes/recipeRoute.mjs
+++ routes/recipeRoute.mjs
@@ -1,11 +1,33 @@
 import express from 'express';
+import Recipe from '../model/recipeSchema.mjs';
 
 const router = express.Router();
 
 
-Test
-router.post('/', (req, res) => {
-    res.send(`This is a test post`)
+// Test
+// router.post('/', (req, res) => {
+//     res.send(`This is a test post`)
+// });
+
+
+// Create
+router.post('/', async (req, res) => {
+    try {
+
+        let newRecipe = new Recipe(req.body);
+
+        await newRecipe.save();
+
+        res.status(200).json(newRecipe);
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({msg: 'Server error'});
+    };
 });
+
+
+
+
 
 export default router;
