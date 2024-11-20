974ee047c6bfa90dd9785aacaafbb32d5dec26c1
dayane112 <71454308+dayane112@users.noreply.github.com>
2024-11-20 11:10:09 -0500

    Created recipeData.mjs and added a recipe

---
+++ data/recipeData.mjs
@@ -0,0 +1,33 @@
+export default recipe = [
+    {
+        name: "Easy Perfect Yeast Bread",
+        category: "baking",
+        level: "medium",
+        serving: 12,
+        total: "90 minutes",
+        ingredient: [
+            "1 tablespoon active dry yeast",
+            "1 tablespoon sugar",
+            "1 tablespoon salt",
+            "2 cups warm water not over 110°F",
+            "5 1/2 to 6 cups All-Purpose Flour",
+            "cornmeal or flour for dusting",
+            "boiling water"
+        ],
+        direction: [
+            "In a large bowl mix together the yeast, sugar, salt and water. Let this stand until the yeast is dissolved. Gradually add the flour, one cup at a time to the liquid and mix thoroughly until the dough pulls away from the sides of the bowl. Turn the dough out onto a floured surface to knead. (This may be a little messy, but don't give up!)",
+            "Knead It: Fold the far edge of the dough back over on itself towards you. Press into the dough with the heels of your hands and push away. After each push, rotate the dough 90°. Repeat this process in a rhythmic, rocking motion for 5 minutes, sprinkling only enough flour on your kneading surface to prevent sticking. Let the dough rest while you scrape out and grease the mixing bowl with a few drops of olive oil (preferred) or non stick baking spray. Knead the dough again for 2 to 3 minutes.",
+            "Let It Rise: Return the dough to the bowl and turn it over once to grease the top. Cover with a damp towel and keep warm until the dough doubles in bulk, about 1 to 2 hours.",
+            "Shape it: Punch down the dough with your fist and briefly knead out any air bubbles. Cut the dough in half and shape into two Italian- or French-style loaves. Place the loaves on a cookie sheet generously sprinkled with cornmeal. Let the loaves rest for 5 minutes.",
+            "Bake it:",
+            "Lightly slash the tops of the loaves 3 or more times diagonally. I use this lame. Brush them lightly with cold water.",
+            "Place an aluminum roasting pan on the bottom of the oven. Fill 1 inch deep with boiling water. Slide loaves onto baking stone in a cold oven. I use this pizza paddle to put the bread into the oven.",
+            "Bake at 400°F for 35 to 45 minutes, until the loaves are golden brown and sounds hollow when tapped.",
+            "Allow bread to cool before slicing. It really helps to have a sharp serrated bread knife for smooth cuts and so the bread doesn't tear.",
+            "Alternate method:",
+            "For a crustier bread, let your shaped loaves rise for 45 minutes. Preheat the oven to 500°F and place an empty cast iron or aluminum roasting pan into the bottom of the oven. Preheat for 15 minutes. Meanwhile heat 1 cup of boiling water on the stove. Brush the loaves with cold water, place in the oven. Pour boiling water into the empty tray. Bake for 10 minutes. Lower the temperature to 400°F and bake for 10 more minutes. Remove from the oven and place on a cooling rack. Let cool for at least 30 minutes before slicing.",
+            "* If you don't have a baking stone, you are welcome to try using an inverted cookie sheet. Please note your bread will not have as crisp of a crust."
+        ]
+    },
+    
+]
